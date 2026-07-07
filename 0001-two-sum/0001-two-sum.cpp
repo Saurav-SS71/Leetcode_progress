0908fd73e918ef10431279da487b21d9ee1438fd
Saurav Kumar <saurav.prof7@gmail.com>
2026-07-07 18:07:33 +0530

    Time: 2 ms (72.34%), Space: 14.7 MB (54.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,14 +5,12 @@ public:
         unordered_map < int, int> dat;
         
         for(int i=0; i<nums.size(); i++){
-            dat[nums[i]]=i;
-        }
-        for(int i=0; i<nums.size(); i++){
-            if(dat.find(target - nums[i])!=dat.end()&&(i!=dat[target-nums[i]])){
-                V.push_back(i);
+            if(dat.find(target - nums[i])!=dat.end()){
                 V.push_back(dat[target-nums[i]]);
+                V.push_back(i);
                 break;
             }
+            dat[nums[i]]=i;
         }
         return V;
     }
