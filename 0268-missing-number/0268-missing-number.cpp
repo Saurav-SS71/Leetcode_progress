71bf96295b6654d36c640d14bfb5facb690805d9
Saurav Kumar <saurav.prof7@gmail.com>
2026-07-31 19:18:24 +0530

    Time: 3 ms (19.3%), Space: 21.8 MB (71.36%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int c=-1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=i){
+                c=i;
+                break;
+            }
+        }
+        if(c==-1){
+            return nums.size();
+        }
+        return c;
+    }
+};
