81d76a584372f7d027b970c68ac584c83a905f38
Saurav Kumar <saurav.prof7@gmail.com>
2026-08-02 22:34:58 +0530

    Time: 3 ms (17.92%), Space: 8.5 MB (51.03%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        long long i=1;
-        long long n=1;
-        while(n<=x){i++;n=i*i;}
-        return (int)(i-1);
+    int mySqrt(int n) {
+        long long L=1,H=n,mid=(L+H)/2,ans=0;
+        while(L<=H){
+            if(mid*mid==n){return mid;}
+            if(mid*mid<n){ans=mid;L=mid+1;}
+            else{H=mid-1;}
+            mid=(L+H)/2;
+        }
+        return (int)(ans);
     }
 };
